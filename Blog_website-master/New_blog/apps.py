5346af7ffba7d6d8020a407c23f3c33c8a7221cd
Katikitala Patanjali <82458375+katikitala@users.noreply.github.com>
2022-07-12 14:12:48 +0530

    Delete Blog_website-master directory

--- Blog_website-master/New_blog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewBlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'New_blog'
--- Blog_website-master/members/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserRegisterView
-
-urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name = 'Register'),
-    # no need to create the url for login page because django authentication take care of it as we included its package
-]
